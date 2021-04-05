0830db5da3a591077417f6717098aafde20d2832
MoeRayo <moronfolu.motunrayo1@gmail.com>
2021-04-05 01:08:49 +0100

    feat: creation of the header and the review cards

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,47 @@
 <template>
-  <div id="app" class="bg-dark-pink">
+  <div id="app" class="relative ph5 pt5 h-100">
+    <img src="./assets/bg-pattern-top-desktop.svg" alt="" class="absolute top-0 left-0">
 
-    goat
+    <section class="flex justify-around flex-column flex-row-ns">
+
+      <section >
+
+        <h1 class="f2 lh-title">10,000+ of our <br> users love our <br> products.</h1>
+        <p class="lh-copy">We only provide great products combined with excellent <br> customer service.
+          See what our satisfied customers <br> are saying about our services.
+        </p>
+      </section>
+        <section class="flex flex-column mt5">
+         <div class="w-75">
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span>Rated 5 Stars in Reviews</span>
+         </div>
+         <div class="ml5 w-90">
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span>Rated 5 Stars in Report Guru</span>
+         </div>
+
+         <div class="ml6 w-90">
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span><img src="./assets/icon-star.svg" alt=""></span> 
+            <span>Rated 5 Stars in BestTech</span>
+         </div>
+        </section>
+      
+    </section>
+    <img src="./assets/bg-pattern-bottom-desktop.svg" alt="" class="absolute right-0 bottom-0">
+
   </div>
 </template>
 
@@ -14,4 +54,35 @@ export default {
 </script>
 
 <style>
+  html,body {
+    height: 100%;
+    /* overflow: hidden; */
+  }
+ 
+  section section:first-of-type h1 {
+    color: hsl(300, 43%, 22%);
+  }
+
+  section section:first-of-type p {
+    color: hsl(303, 10%, 53%);
+  }
+
+  section section:nth-of-type(2) div {
+    background:hsl(300, 24%, 96%);
+    padding: 1rem 2.5rem 1rem 1.5rem ;
+   margin-bottom: 1.5rem;
+    font-weight: 700;
+    color:hsl(300, 43%, 22%);
+    border-radius: 8px;
+  }
+
+  section section:nth-of-type(2) div span:last-child{
+    padding: 0 0 0 1.5rem;
+  }
+
+  body {
+    font-family: 'Spartan', sans-serif;
+    font-size: 15px;
+  }
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '!style-loader!css-loader!tachyons/css/tachyons.css' 
 
 Vue.config.productionTip = false
 
